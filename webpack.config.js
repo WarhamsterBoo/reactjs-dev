9aee461d3250f983f618d8eb90d673c508292281
WarhamsterBoo <warhamsterboo@gmail.com>
2020-04-02 22:02:30 +0300

    add husky + some reformattings

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,25 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.ts',
-    resolve: {
-        extensions: [".js", ".ts"]
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|ts)x?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin()
-    ]
-}
+  entry: "./src/index.ts",
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
