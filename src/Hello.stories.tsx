1e59cda3cdbd453c0576746891db8bfaa9abb64a
WarhamsterBoo <warhamsterboo@gmail.com>
2020-04-12 11:43:52 +0300

    add knobs addon for storybook

--- src/Hello.stories.tsx
+++ src/Hello.stories.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-
 import Hello from "./Hello";
+import { withKnobs, text } from "@storybook/addon-knobs";
 
-export default { title: "Hello component" };
+export default { title: "Hello component", decorators: [withKnobs] };
 
 export const HelloStory: React.FC<{}> = () => {
-  return <Hello name={"me"} />;
+  const userName = text("User name", "me");
+
+  return <Hello name={userName} />;
 };
