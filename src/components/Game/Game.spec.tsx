31ae83ed6b8eaeca0dd5a7501f7d2f9886c9833a
WarhamsterBoo <warhamsterboo@gmail.com>
2020-05-03 13:50:21 +0300

    add tests for zero and negative dimenstions in Game component

--- src/components/Game/Game.spec.tsx
+++ src/components/Game/Game.spec.tsx
@@ -6,6 +6,20 @@ import { Game, WorldPresenter } from "./Game";
 describe("Game", () => {
   const fakeWorld: WorldPresenter = () => null;
 
+  it("should pass an empty creatures list to World if demensions are negative", () => {
+    const sut = mount(
+      <Game xDimension={-1} yDimension={-3} world={fakeWorld} />
+    );
+
+    expect(sut.find(fakeWorld).props().creatures).toEqual([]);
+  });
+
+  it("should pass an empty creatures list to World if demensions are zero", () => {
+    const sut = mount(<Game xDimension={0} yDimension={0} world={fakeWorld} />);
+
+    expect(sut.find(fakeWorld).props().creatures).toEqual([]);
+  });
+
   it("should render initialized World component with size 1 x 1", () => {
     const sut = mount(<Game xDimension={1} yDimension={1} world={fakeWorld} />);
 
@@ -37,7 +51,7 @@ describe("Game", () => {
     );
   });
 
-  it("should be able to increase size without state reset", () => {
+  it("should be able to increase size without creatures state reset", () => {
     const sut = mount(<Game xDimension={2} yDimension={2} world={fakeWorld} />);
     act(() => {
       sut.find(fakeWorld).props().onClick(0, 1);
@@ -56,6 +70,29 @@ describe("Game", () => {
       [{ IsAlive: false }, { IsAlive: true }, { IsAlive: false }],
       [{ IsAlive: false }, { IsAlive: true }, { IsAlive: false }],
       [{ IsAlive: false }, { IsAlive: false }, { IsAlive: false }],
+    ]);
+  });
+
+  it("should be able to decrease size without creatures state reset", () => {
+    const sut = mount(<Game xDimension={3} yDimension={3} world={fakeWorld} />);
+    act(() => {
+      sut.find(fakeWorld).props().onClick(0, 0);
+      sut.find(fakeWorld).props().onClick(1, 1);
+      sut.find(fakeWorld).props().onClick(2, 0);
+    });
+    sut.update();
+    expect(sut.find(fakeWorld).props().creatures).toEqual([
+      [{ IsAlive: true }, { IsAlive: false }, { IsAlive: false }],
+      [{ IsAlive: false }, { IsAlive: true }, { IsAlive: false }],
+      [{ IsAlive: true }, { IsAlive: false }, { IsAlive: false }],
+    ]);
+
+    sut.setProps({ xDimension: 2, yDimension: 2 });
+
+    sut.update();
+    expect(sut.find(fakeWorld).props().creatures).toEqual([
+      [{ IsAlive: true }, { IsAlive: false }],
+      [{ IsAlive: false }, { IsAlive: true }],
     ]);
   });
 });
