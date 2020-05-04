ad81a8c3afed561e571b5474ff3b90ce92ae611a
WarhamsterBoo <warhamsterboo@gmail.com>
2020-05-04 22:17:53 +0300

    make ControlPanel react to click on buttons

--- src/components/Game/components/ControlsPanel/ControlPanel.tsx
+++ src/components/Game/components/ControlsPanel/ControlPanel.tsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { ButtonsContainer, ControlButton } from "./ControlPanel.styled";
 
-export interface ControlPanelProps {}
+export type Action = "stop" | "run" | "pause" | "slower" | "normal" | "faster";
 
-export const ControlPanel: React.FC<ControlPanelProps> = () => {
+export interface ControlPanelProps {
+  onClick: (action: Action) => void;
+}
+
+export const ControlPanel: React.FC<ControlPanelProps> = ({ onClick }) => {
+  const onClickHandler = useCallback(
+    (ev: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      const target = ev.target as HTMLButtonElement;
+      onClick(target.id as Action);
+    },
+    [onClick]
+  );
+
   return (
     <ButtonsContainer>
-      <ControlButton>Stop</ControlButton>
-      <ControlButton>Pause</ControlButton>
-      <ControlButton>Faster</ControlButton>
-      <ControlButton>1x</ControlButton>
-      <ControlButton>Slower</ControlButton>
+      <ControlButton id={"stop"} onClick={onClickHandler}>
+        Stop
+      </ControlButton>
+      <ControlButton id={"run"} onClick={onClickHandler}>
+        Run
+      </ControlButton>
+      <ControlButton id={"pause"} onClick={onClickHandler}>
+        Pause
+      </ControlButton>
+      <ControlButton id={"faster"} onClick={onClickHandler}>
+        Faster
+      </ControlButton>
+      <ControlButton id={"normal"} onClick={onClickHandler}>
+        Normal
+      </ControlButton>
+      <ControlButton id={"slower"} onClick={onClickHandler}>
+        Slower
+      </ControlButton>
     </ButtonsContainer>
   );
 };
