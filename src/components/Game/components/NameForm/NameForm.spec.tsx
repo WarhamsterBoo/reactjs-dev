16817416773fa4854e7340888bb4894111d39fa2
WarhamsterBoo <warhamsterboo@gmail.com>
2020-05-04 23:52:26 +0300

    make NameForm controlled component

--- src/components/Game/components/NameForm/NameForm.spec.tsx
+++ src/components/Game/components/NameForm/NameForm.spec.tsx
@@ -1,3 +1,4 @@
+import { mount } from "enzyme";
 import React from "react";
 import renderer from "react-test-renderer";
 import { NameForm } from "./NameForm";
@@ -7,5 +8,18 @@ describe("Name Form", () => {
     const sut = <NameForm />;
 
     expect(renderer.create(sut).toJSON()).toMatchSnapshot();
+  });
+
+  it("should change value in name input", () => {
+    const sut = mount(<NameForm />);
+
+    sut
+      .find(`input[type="text"]`)
+      .at(0)
+      .simulate("change", {
+        target: { value: "John Doe", name: "name" },
+      });
+
+    expect(sut.find(`input[type="text"]`).at(0).prop("value")).toBe("John Doe");
   });
 });
--- src/components/Game/components/NameForm/NameForm.tsx
+++ src/components/Game/components/NameForm/NameForm.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState, useCallback, FormEvent } from "react";
 import { Button, InputText } from "shared/";
 import { InputsContainer } from "./NameForm.styled";
 
 export const NameForm: React.FC<{}> = () => {
+  const [userName, setUserName] = useState<string>("");
+
+  const onHandleInputChange = useCallback((ev: FormEvent<HTMLInputElement>) => {
+    const { value } = ev.target as HTMLInputElement;
+    setUserName(value);
+  }, []);
+
   return (
     <InputsContainer>
       <form>
@@ -10,7 +17,7 @@ export const NameForm: React.FC<{}> = () => {
           <legend>Hello!</legend>
           <label>
             Name:
-            <InputText />
+            <InputText value={userName} onChange={onHandleInputChange} />
           </label>
           <Button>Ok</Button>
         </fieldset>
