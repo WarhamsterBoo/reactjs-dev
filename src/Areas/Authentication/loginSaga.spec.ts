ace802e95bacee581bffd11e3b1b3745ac38e48d
WarhamsterBoo <warhamsterboo@gmail.com>
2020-06-20 16:36:54 +0300

    login saga should login new user successfully

--- src/Areas/Authentication/loginSaga.spec.ts
+++ src/Areas/Authentication/loginSaga.spec.ts
@@ -1,8 +1,22 @@
 import { expectSaga } from "redux-saga-test-plan";
 import { loginSaga } from "./loginSaga";
+import { authStore, AuthStatus } from "./authStore";
 
 describe("login saga", () => {
-  it("should login user successfully", () => {
-    expect(true).toBeTruthy();
+  it("should login new user successfully", () => {
+    expectSaga(loginSaga)
+      .withReducer(authStore.reducer)
+      .withState({
+        userName: undefined,
+        status: AuthStatus.not_authenticated,
+        loginError: undefined,
+      })
+      .dispatch(authStore.actions.login("John Doe"))
+      .hasFinalState({
+        userName: "John Doe",
+        status: AuthStatus.authenticated,
+        loginError: undefined,
+      })
+      .run();
   });
 });
--- src/Areas/Authentication/loginSaga.ts
+++ src/Areas/Authentication/loginSaga.ts
@@ -1,3 +1,9 @@
+import { auth } from "api/auth";
+import { call, put, take } from "redux-saga/effects";
+import { authStore } from "./authStore";
+
 export function* loginSaga() {
-  yield;
+  const userName = (yield take(authStore.actions.login)).payload;
+  yield call(auth.login, userName);
+  yield put(authStore.actions.login_success);
 }
