e15de927bb2a2dac550050e0a6bc047fe60cfde5
WarhamsterBoo <warhamsterboo@gmail.com>
2020-06-20 16:52:42 +0300

    login saga should restore user successfully

--- src/Areas/Authentication/loginSaga.spec.ts
+++ src/Areas/Authentication/loginSaga.spec.ts
@@ -1,10 +1,17 @@
 import { expectSaga } from "redux-saga-test-plan";
 import { loginSaga } from "./loginSaga";
 import { authStore, AuthStatus } from "./authStore";
+import { userSessionStorage } from "api/userSessionStorage";
+import { call } from "redux-saga-test-plan/matchers";
+import { auth } from "api/auth";
 
 describe("login saga", () => {
   it("should login new user successfully", () => {
-    expectSaga(loginSaga)
+    return expectSaga(loginSaga)
+      .provide([
+        [call(userSessionStorage.getCurrentSession), undefined],
+        [call(auth.login, "John Doe"), {}]
+      ])
       .withReducer(authStore.reducer)
       .withState({
         userName: undefined,
@@ -12,6 +19,26 @@ describe("login saga", () => {
         loginError: undefined,
       })
       .dispatch(authStore.actions.login("John Doe"))
+      .hasFinalState({
+        userName: "John Doe",
+        status: AuthStatus.authenticated,
+        loginError: undefined,
+      })
+      .run();
+  });
+
+  it("should restore user successfully", () => {
+    return expectSaga(loginSaga)
+      .provide([
+        [call(userSessionStorage.getCurrentSession), "John Doe"],
+        [call(auth.login, "John Doe"), {}],
+      ])
+      .withReducer(authStore.reducer)
+      .withState({
+        userName: undefined,
+        status: AuthStatus.not_authenticated,
+        loginError: undefined,
+      })
       .hasFinalState({
         userName: "John Doe",
         status: AuthStatus.authenticated,
--- src/Areas/Authentication/loginSaga.ts
+++ src/Areas/Authentication/loginSaga.ts
@@ -1,9 +1,21 @@
 import { auth } from "api/auth";
-import { call, put, take } from "redux-saga/effects";
+import { call, put, take, fork } from "redux-saga/effects";
 import { authStore } from "./authStore";
+import { userSessionStorage } from "api/userSessionStorage";
+
+export function* restoreCurrentSession() {
+  const currentUsername: string = yield call(userSessionStorage.getCurrentSession);
+  if (currentUsername && currentUsername.length > 0) {
+    yield put(authStore.actions.login(currentUsername))
+  }
+}
 
 export function* loginSaga() {
+  yield fork(restoreCurrentSession);
+
   const userName = (yield take(authStore.actions.login)).payload;
   yield call(auth.login, userName);
   yield put(authStore.actions.login_success);
+
+  
 }
