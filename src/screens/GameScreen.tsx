37a9defc0189b23d3c44f247850be3d58fdf4b32
WarhamsterBoo <warhamsterboo@gmail.com>
2020-06-22 21:30:00 +0300

    delete components alias

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -1,6 +1,6 @@
+import { Header } from "@/components/Header";
 import { WithAuthentication } from "@/Features/Authentication/WithAuthentication";
 import { Game } from "@/Features/Game";
-import { Header } from "components/Header";
 import React from "react";
 import { GameScreenContainer } from "./GameScreen.styled";
 
