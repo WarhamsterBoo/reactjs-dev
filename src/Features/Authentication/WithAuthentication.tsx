e1b1cadebacec98cf19e2ef1c1588eeccf4cf1d5
WarhamsterBoo <warhamsterboo@gmail.com>
2020-06-22 21:28:03 +0300

    delete screens alias

--- src/Features/Authentication/WithAuthentication.tsx
+++ src/Features/Authentication/WithAuthentication.tsx
@@ -1,8 +1,8 @@
 import { AppState } from "@/AppStore";
+import { AuthInProgressScreen } from "@/screens/AuthInProgressScreen";
+import { ForbiddenScreen } from "@/screens/ForbiddenScreen";
 import React from "react";
 import { connect } from "react-redux";
-import { AuthInProgressScreen } from "screens/AuthInProgressScreen";
-import { ForbiddenScreen } from "screens/ForbiddenScreen";
 import { AuthStatus } from "./authStore";
 
 interface WithAuthenticationComponentProps {
