fa07244b137a7dd191ea844ce13f057722fef2dd
WarhamsterBoo <warhamsterboo@gmail.com>
2020-07-14 23:13:56 +0300

    WIP remove state from SettingsForm: implement watchSettingChange

--- src/features/Game/gameSaga.spec.ts
+++ src/features/Game/gameSaga.spec.ts
@@ -1,89 +1,11 @@
 import { testSaga } from "redux-saga-test-plan";
-import { changeSettings } from "./gameSaga";
+import { changeSettings, watchSettingChange } from "./gameSaga";
 import { gameStore } from "./gameStore";
 import { settingsSelector } from "./gameStoreSelectors";
 
 describe("game saga", () => {
   describe("change settings", () => {
-    it("should not save settings if fillingPercentage > 1", () => {
-      const sut = testSaga(
-        changeSettings,
-        gameStore.actions.changeSettingsTo({
-          fillingPercentage: 2,
-          xDimension: 1,
-          yDimension: 2,
-        })
-      );
 
-      sut.next().isDone();
-    });
-
-    it("should not save settings if fillingPercentage < 0", () => {
-      const sut = testSaga(
-        changeSettings,
-        gameStore.actions.changeSettingsTo({
-          fillingPercentage: -1,
-          xDimension: 1,
-          yDimension: 2,
-        })
-      );
-
-      sut.next().isDone();
-    });
-
-    it("should save valid settings and resize creatures if filling percentage does not change", () => {
-      const sut = testSaga(
-        changeSettings,
-        gameStore.actions.changeSettingsTo({
-          fillingPercentage: 0.5,
-          xDimension: 1,
-          yDimension: 2,
-        })
-      );
-
-      sut
-        .next()
-        .select(settingsSelector)
-        .next({ fillingPercentage: 0.5, xDimension: 1, yDimension: 1 })
-        .put(
-          gameStore.actions.saveSettings({
-            fillingPercentage: 0.5,
-            xDimension: 1,
-            yDimension: 2,
-          })
-        )
-        .next()
-        .put(gameStore.actions.resizeCreatures())
-        .next()
-        .isDone();
-    });
-
-    it("should save valid settings and generate new creatures if filling percentage and dimensions change", () => {
-      const sut = testSaga(
-        changeSettings,
-        gameStore.actions.changeSettingsTo({
-          fillingPercentage: 0.6,
-          xDimension: 1,
-          yDimension: 2,
-        })
-      );
-
-      sut
-        .next()
-        .select(settingsSelector)
-        .next({ fillingPercentage: 0.5, xDimension: 1, yDimension: 1 })
-        .put(
-          gameStore.actions.saveSettings({
-            fillingPercentage: 0.6,
-            xDimension: 1,
-            yDimension: 2,
-          })
-        )
-        .next()
-        .put(gameStore.actions.generateNewCreatures())
-        .next()
-        .isDone();
-    });
 
     it("should save valid settings and generate new creatures if only filling percentage changes", () => {
       const sut = testSaga(
@@ -110,6 +32,86 @@ describe("game saga", () => {
         .put(gameStore.actions.generateNewCreatures())
         .next()
         .isDone();
+    });
+  });
+
+  describe("watchSettingChange", () => {
+    it("should not apply settings if fillingPercentage > 1", () => {
+      const sut = testSaga(watchSettingChange);
+
+      sut
+        .next()
+        .select(settingsSelector)
+        .next({ fillingPercentage: 0.5, xDimension: 1, yDimension: 1 })
+        .take(gameStore.actions.applySettings().type)
+        .next()
+        .select(settingsSelector)
+        .next({ fillingPercentage: 1.5, xDimension: 1, yDimension: 1 })
+        .select(settingsSelector);
+    });
+
+    it("should not apply settings if fillingPercentage < 0", () => {
+      const sut = testSaga(watchSettingChange);
+
+      sut
+        .next()
+        .select(settingsSelector)
+        .next({ fillingPercentage: 0.5, xDimension: 1, yDimension: 1 })
+        .take(gameStore.actions.applySettings().type)
+        .next()
+        .select(settingsSelector)
+        .next({ fillingPercentage: -0.1, xDimension: 1, yDimension: 1 })
+        .select(settingsSelector);
+    });
+
+    it("should resize creatures with same state if filling percentage does not change", () => {
+      const sut = testSaga(watchSettingChange);
+
+      sut
+        .next()
+        .select(settingsSelector)
+        .next({ fillingPercentage: 0.5, xDimension: 1, yDimension: 1 })
+        .take(gameStore.actions.applySettings().type)
+        .next()
+        .select(settingsSelector)
+        .next({ fillingPercentage: 0.5, xDimension: 2, yDimension: 2 })
+        .put(gameStore.actions.resizeCreatures())
+        .next()
+        .select(settingsSelector);
+    });
+
+    it("should generate new creatures if filling percentage and dimensions change", () => {
+      const sut = testSaga(watchSettingChange);
+
+      sut
+      sut
+        .next()
+        .select(settingsSelector)
+        .next({ fillingPercentage: 0.5, xDimension: 1, yDimension: 1 })
+        .take(gameStore.actions.applySettings().type)
+        .next()
+        .select(settingsSelector)
+        .next({ fillingPercentage: 0.7, xDimension: 2, yDimension: 2 })
+        .put(gameStore.actions.generateNewCreatures())
+        .next()
+        .select(settingsSelector);
+    });
+
+    it("should generate new creatures if only filling percentage changes", () => {
+      const sut = testSaga(watchSettingChange);
+
+      sut
+      sut
+        .next()
+        .select(settingsSelector)
+        .next({ fillingPercentage: 0.5, xDimension: 1, yDimension: 1 })
+        .take(gameStore.actions.applySettings().type)
+        .next()
+        .select(settingsSelector)
+        .next({ fillingPercentage: 0.7, xDimension: 1, yDimension: 1 })
+        .put(gameStore.actions.generateNewCreatures())
+        .next()
+        .select(settingsSelector);
     });
   });
 });
--- src/features/Game/gameSaga.ts
+++ src/features/Game/gameSaga.ts
@@ -1,5 +1,5 @@
 import { PayloadAction } from "@reduxjs/toolkit";
-import { put, takeEvery, select } from "redux-saga/effects";
+import { put, takeEvery, select, fork, take } from "redux-saga/effects";
 import { GameSettings, gameStore } from "./gameStore";
 import { settingsSelector } from "./gameStoreSelectors";
 
@@ -24,6 +24,28 @@ export function* changeSettings(action: PayloadAction<GameSettings>) {
   }
 }
 
+export function* watchSettingChange() {
+  while(true) {
+    const oldSettings: GameSettings = yield select(settingsSelector);
+    yield take(gameStore.actions.applySettings.type);
+    const newSettings: GameSettings = yield select(settingsSelector);
+    if (
+      newSettings.fillingPercentage < 0 ||
+      newSettings.fillingPercentage > 1
+    ) continue;
+
+    if (oldSettings.fillingPercentage != newSettings.fillingPercentage) {
+      yield put(gameStore.actions.generateNewCreatures());
+    } else if (
+      oldSettings.xDimension != newSettings.xDimension ||
+      oldSettings.yDimension != newSettings.yDimension
+    ) {
+      yield put(gameStore.actions.resizeCreatures());
+    }
+  }
+}
+
 export function* gameSaga() {
+  yield fork(watchSettingChange);
   yield takeEvery(gameStore.actions.changeSettingsTo, changeSettings);
 }
