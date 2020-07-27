48c35123d03aa0184633881639998fa03028d948
WarhamsterBoo <warhamsterboo@gmail.com>
2020-07-27 23:46:54 +0300

    add simple ssr with express and puppeteer

---
+++ ssr/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const puppeteerRenderer = require('puppeteer-renderer-middleware');
+const PORT = 5005;
+
+const app = express();
+
+app.use(puppeteerRenderer({
+  url: 'http://localhost:5005/render',
+}));
+
+app.use(express.static("../dist"));
+
+app.use((_, res) => res.redirect("/"));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
