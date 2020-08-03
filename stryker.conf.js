5bbbb0bc7ff80b7f5247a1b58998b5999b51324a
WarhamsterBoo <warhamsterboo@gmail.com>
2020-08-04 00:18:44 +0300

    add stryker

---
+++ stryker.conf.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+  mutator: "typescript",
+  packageManager: "npm",
+  reporters: ["html", "clear-text", "progress"],
+  testRunner: "jest",
+  transpilers: [],
+  coverageAnalysis: "off",
+  tsconfigFile: "tsconfig.json",
+  mutate: ["src/**/*.ts"],
+  jest: {
+    config: require(__dirname + '/tests/jest.config.js')
+  },
+  timeoutMS: 20000,
+};
