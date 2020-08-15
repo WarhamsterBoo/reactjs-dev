5e81cb5d879b7842a6f68a8a88e9879c266bb11e
WarhamsterBoo <warhamsterboo@gmail.com>
2020-08-15 12:55:20 +0300

    add final project requirements to readme

--- ssr/index.js
+++ ssr/index.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const puppeteerRenderer = require('puppeteer-renderer-middleware');
+const express = require("express");
+const puppeteerRenderer = require("puppeteer-renderer-middleware");
 const PORT = 5005;
 
 const app = express();
 
-app.use(puppeteerRenderer({
-  url: 'http://localhost:5005/render',
-}));
+app.use(
+  puppeteerRenderer({
+    url: "http://localhost:5005/render",
+  })
+);
 
 app.use(express.static("../dist"));
 
