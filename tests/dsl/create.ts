004cfa1e701c68f248d3a17e959b3139888bb608
WarhamsterBoo <warhamsterboo@gmail.com>
2020-08-15 13:19:31 +0300

    add tests/dsl dirrectory to lint command

--- tests/dsl/create.ts
+++ tests/dsl/create.ts
@@ -4,45 +4,45 @@ import { GameSettings, GameStatus, GameState } from "@/features/Game/gameStore";
 import configureMockStore from "redux-mock-store";
 
 export const create = {
-    defaultAuthState: () => ({
-        loginError: undefined,
-        status: AuthStatus.not_authenticated,
-        userName: undefined,
-    }),
-    gameSettings: (overrides?: Partial<GameSettings>): GameSettings => ({
-        xDimension: 10,
-        yDimension: 10,
-        fillingPercentage: 0,
-        speed: 10,
-        status: GameStatus.Stopped,
-        ...overrides
-    }),
-    authState: (overrides?: Partial<AuthState>): AuthState => ({
-        loginError: undefined,
-        userName: "John Doe",
-        status: AuthStatus.authenticated,
-        ...overrides
-    }),
-    gameState: (overrides?: Partial<GameState>): GameState => ({
+  defaultAuthState: () => ({
+    loginError: undefined,
+    status: AuthStatus.not_authenticated,
+    userName: undefined,
+  }),
+  gameSettings: (overrides?: Partial<GameSettings>): GameSettings => ({
+    xDimension: 10,
+    yDimension: 10,
+    fillingPercentage: 0,
+    speed: 10,
+    status: GameStatus.Stopped,
+    ...overrides,
+  }),
+  authState: (overrides?: Partial<AuthState>): AuthState => ({
+    loginError: undefined,
+    userName: "John Doe",
+    status: AuthStatus.authenticated,
+    ...overrides,
+  }),
+  gameState: (overrides?: Partial<GameState>): GameState => ({
+    creatures: [],
+    settings: create.gameSettings(),
+    ...overrides,
+  }),
+  appState: (overrides?: Partial<AppState>): AppState => ({
+    auth: create.authState(),
+    game: {
+      creatures: [],
+      settings: create.gameSettings(),
+    },
+    ...overrides,
+  }),
+  mockStore: (stateOverrides?: Partial<AppState>) =>
+    configureMockStore<AppState>([])({
+      auth: create.defaultAuthState(),
+      game: {
         creatures: [],
         settings: create.gameSettings(),
-        ...overrides
+      },
+      ...stateOverrides,
     }),
-    appState: (overrides?: Partial<AppState>): AppState => ({
-        auth: create.authState(),
-        game: {
-            creatures: [],
-            settings: create.gameSettings()
-        },
-        ...overrides
-    }),
-    mockStore: (stateOverrides?: Partial<AppState>) =>
-        configureMockStore<AppState>([])({
-            auth: create.defaultAuthState(),
-            game: {
-                creatures: [],
-                settings: create.gameSettings(),
-            },
-            ...stateOverrides
-        }),
-}
+};
--- tests/dsl/mountWithMockStore.tsx
+++ tests/dsl/mountWithMockStore.tsx
@@ -4,14 +4,13 @@ import { Provider } from "react-redux";
 import { create } from "tests/dsl/create";
 import { AppState } from "@/AppStore";
 
-export const mountWithMockStore = (Component: JSX.Element, stateOverrides?: Partial<AppState>) => {
-    const store = create.mockStore(stateOverrides);
-    return {
-        sut: mount(
-            <Provider store={store} >
-                {Component}
-            </ Provider>
-        ),
-        store
-    }
-}
+export const mountWithMockStore = (
+  Component: JSX.Element,
+  stateOverrides?: Partial<AppState>
+) => {
+  const store = create.mockStore(stateOverrides);
+  return {
+    sut: mount(<Provider store={store}>{Component}</Provider>),
+    store,
+  };
+};
