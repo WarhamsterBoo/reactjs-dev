068d6ca14253ba5be3c671b7bfbe6c9652f1b34e
WarhamsterBoo <warhamsterboo@gmail.com>
2020-08-23 22:07:14 +0300

    implement all azure authenticate functions

--- azure-app/LogIn.cs
+++ azure-app/LogIn.cs
@@ -4,6 +4,9 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using System.Linq;
+using Microsoft.Azure.Documents.Client;
+using System;
 
 namespace Company.Function
 {
@@ -12,17 +15,33 @@ namespace Company.Function
         [FunctionName("LogIn")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(databaseName: "game",
+                collectionName: "users",
+                ConnectionStringSetting = "CosmosDBConnection")] DocumentClient client,
             ILogger log)
         {
-            string name = req.Query["name"];
-
-            log.LogInformation($"Start authentication for user: {name}");
-
-            if (name == "Alice" || name == "Bob")
+            var name = req.Query["name"];
+            if (string.IsNullOrWhiteSpace(name))
             {
-                return new OkResult();
+                return new StatusCodeResult(403);
             }
 
+            log.LogInformation($"Logging in {name}...");
+
+            User user = client.CreateDocumentQuery<User>(UriFactory.CreateDocumentCollectionUri("game", "users"))
+                .Where(p => p.Name.Equals(name))
+                .AsEnumerable()
+                .SingleOrDefault();
+
+            if (user != null)
+            {
+                user.IsLoggedIn = true;
+                await client.ReplaceDocumentAsync(UriFactory.CreateDocumentUri("game", "users", user.Id.ToString()), user);
+                log.LogInformation($"User {name} logged in!");
+                return new OkResult();
+            };
+
+            log.LogInformation($"Access Denied for user {name}.");
             return new StatusCodeResult(403);
         }
     }
--- azure-app/Register.cs
+++ azure-app/Register.cs
@@ -1,11 +1,10 @@
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System;
 
 namespace Company.Function
 {
@@ -14,13 +13,20 @@ namespace Company.Function
         [FunctionName("Register")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(databaseName: "game",
+                collectionName: "users",
+                ConnectionStringSetting = "CosmosDBConnection")] IAsyncCollector<User> documents,
             ILogger log)
         {
-            log.LogInformation("Register new user");
+            string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            var name = data?.name;
+            if (string.IsNullOrWhiteSpace(name)) {
+                throw new Exception("name cannot be empty or whitespace");
+            }
+
+            log.LogInformation($"Registering new user {name}...");
+
+            await documents.AddAsync(new User { Name = name, IsLoggedIn = false });
 
             return new OkResult();
         }
