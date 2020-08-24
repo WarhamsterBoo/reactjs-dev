76cb58cf1350ae03364b116d57660d8117d19b0d
WarhamsterBoo <warhamsterboo@gmail.com>
2020-08-25 00:20:07 +0300

    fix Register funciton

--- azure-app/Register.cs
+++ azure-app/Register.cs
@@ -5,6 +5,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System;
+using Microsoft.Azure.Documents.Client;
+using System.Linq;
 
 namespace Company.Function
 {
@@ -15,18 +17,30 @@ namespace Company.Function
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             [CosmosDB(databaseName: "game",
                 collectionName: "users",
-                ConnectionStringSetting = "CosmosDBConnection")] IAsyncCollector<User> documents,
+                ConnectionStringSetting = "CosmosDBConnection")] DocumentClient client,
             ILogger log)
         {
             string name = req.Query["name"];
 
-            if (string.IsNullOrWhiteSpace(name)) {
+            if (string.IsNullOrWhiteSpace(name))
+            {
                 throw new Exception("name cannot be empty or whitespace");
+            }
+
+            User existingUser = client.CreateDocumentQuery<User>(UriFactory.CreateDocumentCollectionUri("game", "users"))
+                .Where(p => p.Name.Equals(name))
+                .AsEnumerable()
+                .SingleOrDefault();
+
+            if (existingUser != null)
+            {
+                throw new Exception("user already exists");
             }
 
             log.LogInformation($"Registering new user {name}...");
 
-            await documents.AddAsync(new User { Name = name, IsLoggedIn = false });
+            User newUser = new User { Id = Guid.NewGuid(), Name = name, IsLoggedIn = false };
+            await client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri("game", "users"), newUser);
 
             return new OkResult();
         }
